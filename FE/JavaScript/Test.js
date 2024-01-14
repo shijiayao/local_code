a5d2ded26648fbee1b6c1458f6c73990470938c1
施佳尧 <shijiayao@example.com>
2024-01-14 13:56:34 +0800

    update

--- FE/JavaScript/Test.js
+++ FE/JavaScript/Test.js
@@ -27,3 +27,200 @@
         return this._fillText(...p);
     };
 }
+
+{
+    /**
+     * menhu.pt.ouchn.cn
+     * 2234001205640
+     * Ouchn@19950320
+     */
+
+    let menuElement = document.querySelector('.full-screen-mode-sidebar'); // 目录
+    let titleLevel_0 = menuElement.querySelectorAll('[style="--level: 0;"]'); // 0 级标题
+    let titleLevel_1 = document.querySelectorAll('[style="--level: 1;"]'); // 0 级标题下展开的 1 级标题
+    let titleLevel_2 = document.querySelectorAll('[style="--level: 2;"]'); // 1 级标题下的 2 级标题
+
+    let activeTitleLevel_0 = null; // 当前展开的 0 级标题
+    let activeTitleLevel_1 = null; // 当前展开的 1 级标题
+    let activeTitleLevel_2 = null; // 当前选中的 2 级标题
+    let activeTitleLevel_0_index = 0; // 当前展开的 0 级标题下标
+    let activeTitleLevel_1_index = 0; // 当前展开的 1 级标题下标
+    let activeTitleLevel_2_index = 0; // 当前选中的 2 级标题下标
+
+    [].forEach.call(titleLevel_2, (element, index) => {
+        if (element.classList.contains('active')) {
+            activeTitleLevel_2 = element;
+            activeTitleLevel_1 = $(element).parents('[style="--level: 1;"]')[0];
+            activeTitleLevel_0 = $(element).parents('[style="--level: 0;"]')[0];
+
+            activeTitleLevel_2_index = index;
+            activeTitleLevel_1_index = $(activeTitleLevel_1).index();
+            activeTitleLevel_0_index = $(activeTitleLevel_0).index();
+        }
+    });
+
+    checkVideo();
+
+    /**
+     * 检查视频
+     */
+    function checkVideo () {
+        console.log('检查视频');
+
+        // 检查视频播放情况
+        let videoElement = document.querySelector('.video-player');
+
+        if (videoElement) {
+            // 是视频
+
+            let videoPlayProgress = videoElement.querySelector('.mvp-play-progress');
+
+            if (parseFloat(videoPlayProgress.style.width) >= 99) {
+                // 视频播放完成
+                clickNextTitle_2();
+            } else {
+                let videoObject = videoElement.querySelector('video');
+
+                videoObject.addEventListener('ended', function (e) {
+                    // 视频播放完成
+                    clickNextTitle_2();
+                });
+
+                setTimeout(() => {
+                    videoElement.querySelector('.mvp-toggle-play').click();
+
+                    setTimeout(() => {
+                        if (videoObject.paused) {
+                            videoElement.querySelector('.mvp-toggle-play').click();
+                        }
+                    }, 5000);
+                }, 3000);
+            }
+        } else {
+            // 不是视频，点击下一个标题
+            clickNextTitle_2();
+        }
+    }
+
+    /**
+     * 点击下一个 2 级标题
+     */
+    function clickNextTitle_2 () {
+        console.log('点击 2 级标题', activeTitleLevel_2_index, titleLevel_2.length);
+
+        if (activeTitleLevel_2_index >= titleLevel_2.length - 1) {
+            // 当前是最后一个 2 级标题，点击下一个 1 级标题
+            clickNextTitle_1();
+        } else {
+            // 点击下一个标题
+            titleLevel_2[activeTitleLevel_2_index + 1].querySelector('.text-too-long').click();
+            activeTitleLevel_2_index++;
+
+            setTimeout(() => {
+                checkVideo();
+            }, 10000);
+        }
+    }
+
+    /**
+     * 点击下一个 1 级标题
+     */
+    function clickNextTitle_1 () {
+        console.log('点击 1 级标题', activeTitleLevel_1_index, titleLevel_1.length);
+
+        if (activeTitleLevel_1_index >= titleLevel_1.length - 1) {
+            // 当前是最后一个 1 级标题，点击下一个 0 级标题
+            clickNextTitle_0();
+        } else {
+            // 点击下一个标题
+
+            titleLevel_1[activeTitleLevel_1_index + 1].querySelector('.text-too-long').click();
+            activeTitleLevel_1_index++;
+            activeTitleLevel_2_index = -1;
+
+            setTimeout(() => {
+                titleLevel_2 = titleLevel_1[activeTitleLevel_1_index].querySelectorAll('[style="--level: 2;"]') || [];
+                clickNextTitle_2();
+            }, 10000);
+        }
+    }
+
+    /**
+     * 点击下一个 0 级标题
+     */
+    function clickNextTitle_0 () {
+        console.log('点击 0 级标题', activeTitleLevel_0_index, titleLevel_0.length);
+
+        if (activeTitleLevel_0_index >= titleLevel_0.length - 1) {
+            // 最后一个 0 级标题
+            console.log('当前页面视频已播放完。');
+        } else {
+            // 点击下一个标题
+
+            titleLevel_0[activeTitleLevel_0_index + 1].querySelector('.text-too-long').click();
+            activeTitleLevel_0_index++;
+            activeTitleLevel_1_index = -1;
+            activeTitleLevel_2_index = -1;
+
+            setTimeout(() => {
+                titleLevel_1 = titleLevel_0[activeTitleLevel_0_index].querySelectorAll('[style="--level: 1;"]') || [];
+                clickNextTitle_1();
+            }, 30000);
+        }
+    }
+}
+
+{
+    let DelayTime = 30 * 1000;
+
+    function checkVideo () {
+        console.log('检查视频');
+
+        // 检查视频播放情况
+        let videoElement = document.querySelector('.video-player');
+
+        if (videoElement) {
+            // 是视频
+
+            let videoPlayProgress = videoElement.querySelector('.mvp-play-progress');
+
+            if (parseFloat(videoPlayProgress.style.width) >= 99) {
+                // 视频已播放
+                console.log('视频已播放。');
+                nextButton();
+            } else {
+                let videoObject = videoElement.querySelector('video');
+
+                videoObject.addEventListener('ended', function (e) {
+                    // 视频播放完成
+                    console.log('视频播放完成。');
+                    nextButton();
+                });
+
+                setTimeout(() => {
+                    videoElement.querySelector('.mvp-toggle-play').click();
+
+                    setTimeout(() => {
+                        if (videoObject.paused) {
+                            videoElement.querySelector('.mvp-toggle-play').click();
+                        }
+                    }, 5000);
+                }, 3000);
+            }
+        } else {
+            // 不是视频，点击下一个
+            nextButton();
+        }
+    }
+
+    // 下一个按钮
+    function nextButton () {
+        document.querySelector('.next-btn.ivu-btn').click();
+
+        setTimeout(() => {
+            checkVideo();
+        }, DelayTime);
+    }
+
+    checkVideo();
+}
---
+++ powershell/auto.ps1
@@ -0,0 +1,179 @@
+﻿$global:whileFlag = 1
+$global:whileTime = 3
+
+$global:openTime = -1
+$global:fristTime = 0
+$global:lastTime = 0
+
+# 获取当前日期时间
+function GetDateTime {
+    $currentDateTime = Get-Date
+
+    return @{
+        year       = $currentDateTime.Year;
+        month      = "{0:00}" -f $currentDateTime.Month;
+        day        = "{0:00}" -f $currentDateTime.Day;
+        hour       = "{0:00}" -f $currentDateTime.Hour;
+        minute     = "{0:00}" -f $currentDateTime.Minute;
+        second     = "{0:00}" -f $currentDateTime.Second;
+        FormatDate = $currentDateTime.ToString("yyyy-MM-dd")
+        FormatTime = $currentDateTime.ToString("HH:mm:ss")
+    }
+}
+
+# 日志
+function log {
+    param($content)
+
+    $CurrentDateTime = GetDateTime
+
+    $filePath = "./log/" + $CurrentDateTime.FormatDate + ".txt"
+    $text = $CurrentDateTime.FormatDate + " " + $CurrentDateTime.FormatTime + " " + $content
+
+    Add-Content -Value $text -Encoding UTF8 -Path $filePath
+
+    Write-Host $text
+}
+
+function info {
+    param($content)
+
+    $CurrentDateTime = GetDateTime
+
+    $filePath = "./log/" + $CurrentDateTime.FormatDate + "-info.txt"
+    $text = $CurrentDateTime.FormatDate + " " + $CurrentDateTime.FormatTime + " " + $content
+
+    Add-Content -Value $text -Encoding UTF8 -Path $filePath
+
+    Write-Host $text
+}
+
+# 查询窗口标题
+function queryApp {
+    $windows = Get-Process | Where-Object { $_.MainWindowTitle -ne "" }
+
+    foreach ($window in $windows) {
+        <#
+        Write-Host "进程名称: $($window.Name)"
+        Write-Host "ID: $($window.Id)"
+        Write-Host "Handle: $($window.Handle)"
+        Write-Host "主窗口标题: $($window.MainWindowTitle)"
+        Write-Host "------------------------"
+        #>
+
+        if ($window.MainWindowTitle -match "巡查辅助程序") {
+            if ($global:openTime -eq -1) {
+
+                $CurrentDateTime = GetDateTime
+
+                $global:openTime = $CurrentDateTime.minute
+
+                if ($global:openTime -lt 30) {
+                    $global:fristTime = $global:openTime
+                    $global:lastTime = $global:openTime + 30
+                }
+                else {
+                    $global:fristTime = $global:openTime - 30
+                    $global:lastTime = $global:openTime
+                }
+
+                info ("openTime: " + $global:openTime)
+                info ("fristTime: " + $global:fristTime)
+                info ("lastTime: " + $global:lastTime)
+
+            }
+
+            Start-Sleep -Seconds (1, 2, 3, 4 | Get-Random)
+
+            clickPoint $window.MainWindowTitle
+        }
+    }
+}
+
+
+function clickPoint {
+    param($title)
+
+    $wshell = New-Object -ComObject wscript.shell
+    $wshell.AppActivate($title)
+    Start-Sleep -Seconds 0.5
+    <#
+    $wshell.SendKeys('{TAB}')
+    Start-Sleep -Seconds 0.5
+    $wshell.SendKeys('{ENTER}')
+    #>
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")
+    [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
+    for ($i = 1; $i -le (1, 2, 3, 4 | Get-Random); $i++) {
+        Start-Sleep -Seconds 0.3
+        [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+    }
+
+    log "点击签到"
+}
+
+function intervalFuzzy {
+    $CurrentDateTime = GetDateTime
+
+    if ($CurrentDateTime.minute -lt 10) {
+        $global:whileTime = (10 - $CurrentDateTime.minute) * 60 - $CurrentDateTime.second
+    }
+    elseif ($CurrentDateTime.minute -ge 20 -and $CurrentDateTime.minute -lt 40) {
+        $global:whileTime = (60 - $CurrentDateTime.minute) * 60 - $CurrentDateTime.second
+    }
+    elseif ($CurrentDateTime.minute -ge 50) {
+        $global:whileTime = (60 - $CurrentDateTime.minute) * 60 + 10 * 60 - $CurrentDateTime.second
+    }
+    else {
+        $global:whileTime = 3
+    }
+
+}
+
+function intervalAccurate {
+    $CurrentDateTime = GetDateTime
+
+    $a = $global:fristTime - 2
+    $b = $global:fristTime + 2
+    $c = $global:lastTime - 2
+    $d = $global:lastTime + 2
+
+    if ($CurrentDateTime.minute -lt $a ) {
+        $global:whileTime = ($a - $CurrentDateTime.minute) * 60 - $CurrentDateTime.second
+    }
+    elseif ($CurrentDateTime.minute -ge $b -and $CurrentDateTime.minute -lt $c) {
+        $global:whileTime = ($c - $CurrentDateTime.minute) * 60 - $CurrentDateTime.second
+    }
+    elseif ($CurrentDateTime.minute -ge $d) {
+        $global:whileTime = (60 - $CurrentDateTime.minute) * 60 + $a * 60 - $CurrentDateTime.second
+    }
+    else {
+        $global:whileTime = 3
+    }
+
+}
+
+log "启动"
+
+while ($global:whileFlag) {
+    $CurrentDateTime = GetDateTime
+
+    queryApp
+
+    if ($global:openTime -eq -1) {
+        intervalFuzzy
+    }
+    else {
+        intervalAccurate
+    }
+
+    if ($CurrentDateTime.hour -ge 20) {
+        $global:whileFlag = 0
+        $global:whileTime = 1
+        log "结束"
+    }
+
+    info ("检测中 " + $global:whileTime)
+
+    Start-Sleep -Seconds $global:whileTime
+}
